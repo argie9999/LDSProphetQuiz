79f430ba99af502c1f7008a4cfd7dc78f3540091
Caleb Robinson <caleb@Bryans-MacBook.local>
2015-11-26 13:48:11 -0700

    Adding questions

--- LDSProphetQuiz/LDSProphetQuiz/ViewController.swift
+++ LDSProphetQuiz/LDSProphetQuiz/ViewController.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    let questions: [String] = [
-        "who is this prophet above",
-        "how old was Thomas S. Monson when he became a bishop",
-        "how many prophets have there been"
+class ViewController: UIViewController {    
+    
+    var questionDict: [String: String] = [
+        "Who is this prophet above?": "Thomas S. Monson",
+        "How old was Thomas S. Monson when he became a bishop?": "22",
+        "How many prophets have there been?": "16",
+        "Who played baseball when he was a kid?": "Heber J. Grant",
+        "Who looked at a kid and wiggeled his ears?": "Thomas S. Monson",
+        "How old was Joseph Smith when he saw Heavenly Father and Jesus Christ?": "14",
+        "Who had open heart surgery when he was a prophet?": "Spencer W. Kimball",
+        "Who was the prophet when the proclamation to the world came out?": "Gordon B. Hinckley"
     ]
+    
     
     var questionIndex: Int = 0
     
     
     @IBOutlet weak var goLabel: UILabel!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,12 +42,33 @@ class ViewController: UIViewController {
 
 
     @IBAction func handleGo(sender: AnyObject) {
-        if questionIndex > questions.count - 1 {
+        if questionIndex > questionDict.count - 1 {
             questionIndex = 0
         }
         
-        goLabel.text = questions[questionIndex]
-        questionIndex++
+        if let (question, answer) = questionDict[questionIndex] {
+            goLabel.text = question
+            answerLabel.text = answer
+            questionIndex++
+        }
     }
 }
 
+
+extension Dictionary {
+    
+    subscript(i: Int) -> (key: Key, value: Value)? {
+        
+        var index = 0
+        
+        for (key, value) in self {
+            if i == index {
+                return (key: key, value: value)
+            }
+            index++
+        }
+        
+        return nil
+    }
+    
+}
